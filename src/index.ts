4daf44f9284dbfeaa48a31100f9860acc4f938ad
Cinnamonsroll <cinnamonsrowolls@gmail.com>
2026-08-04 04:08:40 -0400

    Refactor font registration logging and remove unused imports

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,6 @@ import {
   loadInteractionHandlers,
   interactionsDir,
 } from "./utils/loaders/interactions.js";
-import { existsSync } from "node:fs";
 
 declare module "discord.js" {
   interface Client {
@@ -72,17 +71,7 @@ async function main(): Promise<void> {
       join(__dirname, "images", "assets", "fonts", "SegoeUIBold.ttf"),
       "Segoe UI",
     );
-    // console.log("Fonts registered", GlobalFonts.families);
-    const fontPath = join(
-  __dirname,
-  "images",
-  "assets",
-  "fonts",
-  "SegoeUI.ttf",
-);
-
-console.log(fontPath);
-console.log(existsSync(fontPath));
+    console.log("Fonts registered", GlobalFonts.families);
     logger.info("Registering global slash commands");
     await rest.put(Routes.applicationCommands(env.CLIENT_ID), {
       body: commands.map((cmd) => cmd.data.toJSON()),
